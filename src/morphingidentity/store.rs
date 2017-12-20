8e4d06d46a1821dcc35c1508a066ac6fe8220c36
raphaelrobert <raphael@wire.com>
2017-12-20 17:00:29 +0100

    Add remove function to store

--- src/morphingidentity/store.rs
+++ src/morphingidentity/store.rs
@@ -1,7 +1,7 @@
-use entries::{JournalEntry, EntryType, DeviceType};
 use journal::{FullJournal, JournalID, UserID};
 use std::collections::HashMap;
 
+#[derive(Default)]
 pub struct JournalStore {
     pub journal_id_by_user_id: HashMap<UserID, JournalID>,
     pub journal_by_journal_id: HashMap<JournalID, FullJournal>,
@@ -9,23 +9,24 @@ pub struct JournalStore {
 
 impl JournalStore {
     pub fn new() -> JournalStore {
-        JournalStore {
-            journal_id_by_user_id: HashMap::new(),
-            journal_by_journal_id: HashMap::new(),
+        Default::default()
+    }
+    pub fn get_journal_by_journal_id(&self, j: JournalID) -> Option<&FullJournal> {
+        self.journal_by_journal_id.get(&j)
+    }
+    pub fn get_journal_by_user_id(&self, u: UserID) -> Option<&FullJournal> {
+        self.journal_by_journal_id.get(self.journal_id_by_user_id.get(&u)?)
+    }
+    pub fn insert_or_replace_journal(&mut self, u: UserID, j: &FullJournal) -> Option<FullJournal> {
+        if let Some(x) = self.journal_id_by_user_id.insert(u, j.get_journal_id()) {
+            self.journal_by_journal_id.remove(&x);
+        };
+        self.journal_by_journal_id.insert(j.get_journal_id(), j.clone())
+    }
+    pub fn remove_journal_by_user_id(&mut self, u: UserID) -> Option<FullJournal> {
+        match self.journal_id_by_user_id.remove(&u) {
+            Some(x) => self.journal_by_journal_id.remove(&x),
+            None => None,
         }
     }
-    pub fn get_journal_by_journal_id(&self, j: &JournalID) -> Option<&FullJournal> {
-        self.journal_by_journal_id.get(j)
-    }
-    pub fn get_journal_by_user_id(&self, u: &UserID) -> Option<&FullJournal> {
-        self.journal_by_journal_id.get(self.journal_id_by_user_id.get(u)?)
-    }
-    // pub fn insert_or_replace_journal(&mut self, u: UserID, j: &FullJournal) -> bool {
-    // match self.journal_id_by_user_id.insert(u, j.get_journal_id()) {
-    // Some(x) => self.journal_by_journal_id.remove(&x),
-    // };
-    // self.journal_by_journal_id.insert(j.get_journal_id(), j.clone());
-    // true
-    // }
-    //
 }
