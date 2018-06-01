242a03512d485807adb989779aaadf698cc8419f
Artyom Kazak <yom@artyom.me>
2018-06-01 20:18:16 +0200

    Cosmetics

--- src/morphingidentity/journal.rs
+++ src/morphingidentity/journal.rs
@@ -139,8 +139,10 @@ impl FullJournal {
         Validator::validate_entry(&self, entry)
     }
 
+    /// Add an entry to the journal without validating it.
     fn unchecked_add_entry(&mut self, entry: JournalEntry) {
         self.entries.push(entry.clone());
+        self.hash = entry.hash();
         match entry.operation {
             Operation::DeviceAdd {
                 subject,
@@ -189,20 +191,15 @@ impl FullJournal {
                 );
             }
         };
-        self.hash = self.entries.last().unwrap().hash();
     }
 
     pub fn add_entry(
         &mut self,
         entry: JournalEntry,
     ) -> Result<(), ValidatorError> {
-        match self.can_add_entry(&entry) {
-            Ok(()) => {
-                self.unchecked_add_entry(entry);
-                Ok(())
-            }
-            Err(e) => Err(e),
-        }
+        self.can_add_entry(&entry)?;
+        self.unchecked_add_entry(entry);
+        Ok(())
     }
 
     pub fn as_bytes(&self) -> Vec<u8> {
